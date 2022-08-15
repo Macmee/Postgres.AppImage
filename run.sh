987ef1a3fe9bb1b5aaf01b0b104641a6dd5b2aa8
David Zorychta <macmee@gmail.com>
2022-08-14 23:15:49 -0700

    test

--- run.sh
+++ run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 set -x
+set -m
 
 allArgs="$@" # cute little helper to read "--name value" arguments!
 arg() { echo $allArgs | grep "\-\-$1 " | grep -Ev "\-\-$1 \-\-[a-z]" | sed -e "s/^.*\-\-$1 \([^ ^$]*\).*$/\1/" ; }
 
 
 BASE="$APPDIR/root"
+BIN="$BASE/usr/lib/postgresql/12/bin"
 
-customEnv="PATH=$PATH:$BASE/usr/lib/postgresql/12/bin LD_LIBRARY_PATH=$BASE/usr/lib"
+customEnv="PATH=$PATH:$BIN LD_LIBRARY_PATH=$BASE/usr/lib"
 
 # some distros (fedora docker image) dont have su so we need to use sudo
 
@@ -19,7 +21,7 @@ suCmd="su"
 
 if ! [ "$1" = "" ] && [ -f "$BASE/usr/lib/postgresql/12/bin/$1" ]; then
   args="${@:2}"
-  eval $customEnv $1 $args
+  eval $customEnv $BIN/$1 $args
   exit $?
 fi
 
@@ -82,8 +84,8 @@ dbPath=$([ "$(arg path)" = "" ] && echo "$TEMP_DB_PATH" || arg path)
 chown -R "$userRunningAs" $dbPath || sudo chown -R "$userRunningAs" $dbPath
 rm -f $dbPath/.DS_Store
 shouldInitDb=$([ -z "$(ls -A $dbPath)" ] && echo true || echo false)
-$shouldInitDb && $suCmd "$userRunningAs" bash -c "$customEnv initdb --locale=$useLocale -D $dbPath --noclean" && printf "host all all 0.0.0.0/0 md5\nlocal all all trust\n" >> "$dbPath/pg_hba.conf"
-! [ -d "$dbPath/stats" ] && $suCmd postgres bash -c "mkdir $dbPath/stats"
+$shouldInitDb && $suCmd "$userRunningAs" bash -c "$customEnv $BIN/initdb --locale=$useLocale -D $dbPath --noclean" && printf "host all all 0.0.0.0/0 md5\nlocal all all trust\n" >> "$dbPath/pg_hba.conf"
+! [ -d "$dbPath/stats" ] && $suCmd $userRunningAs bash -c "mkdir $dbPath/stats"
 
 # need to make sure address can connect
 
@@ -91,33 +93,41 @@ $shouldInitDb && $suCmd "$userRunningAs" bash -c "$customEnv initdb --locale=$us
 
 # Resolve the log file
 
-TEMP_LOG_PATH=$(mktemp /tmp/postgresql-logs-XXXX)
-touch $TEMP_LOG_PATH
-chown "$userRunningAs" $TEMP_LOG_PATH || sudo chown "$userRunningAs" $TEMP_LOG_PATH
-logFile=$([ "$(arg log)" = "" ] && echo "$TEMP_LOG_PATH" || arg log)
+#TEMP_LOG_PATH=$(mktemp /tmp/postgresql-logs-XXXX)
+#touch $TEMP_LOG_PATH
+#chown "$userRunningAs" $TEMP_LOG_PATH || sudo chown "$userRunningAs" $TEMP_LOG_PATH
+#logFile=$([ "$(arg log)" = "" ] && echo "$TEMP_LOG_PATH" || arg log)
 
 # start! LD_DEBUG=libs
 
-$suCmd $userRunningAs bash -c "$customEnv pg_ctl start -o '-c config_file=$CONFIG_FILE' -D $dbPath -l $logFile"
+$suCmd $userRunningAs bash -c "$customEnv $BIN/postgres -c config_file=$CONFIG_FILE -D $dbPath" &
+PID=$?!
+
+for i in {1..20}; do
+  $suCmd "$userRunningAs" bash -c "$customEnv $BIN/psql -c \"select now();\"" && break
+  sleep 0.5
+done
 
 # make database, user, password if needed
 
 if $shouldInitDb || ! [ "$(arg database)" = "" ]; then
   dbName=$([ "$(arg database)" = "" ] && echo "postgres" || arg database)
-  $suCmd $userRunningAs bash -c "$customEnv createdb $dbName"
+  $suCmd $userRunningAs bash -c "$customEnv $BIN/createdb $dbName"
 fi
 
 if $shouldInitDb || ! [ "$(arg username)" = "" ]; then
   dbUsername=$([ "$(arg username)" = "" ] && echo "username" || arg username)
   dbPassword=$([ "$(arg password)" = "" ] && echo "password" || arg password)
-  $suCmd "$userRunningAs" bash -c "$customEnv psql -c \"create role $dbUsername with login password '$dbPassword'; alter user $dbUsername with SUPERUSER;\""
+  $suCmd "$userRunningAs" bash -c "$customEnv $BIN/psql -c \"create role $dbUsername with login password '$dbPassword'; alter user $dbUsername with SUPERUSER;\""
 fi
 
 function siginthandler() {
-  $suCmd "$userRunningAs" bash -c "$customEnv pg_ctl stop -o '-c config_file=$CONFIG_FILE' -D $dbPath"
-  rm -rf $CONFIG_FILE $TEMP_DB_PATH $TEMP_LOG_PATH
+  #$suCmd "$userRunningAs" bash -c "$customEnv $BIN/pg_ctl stop -o '-c config_file=$CONFIG_FILE' -D $dbPath"
+  kill -15 $PID
+  rm -rf $CONFIG_FILE $TEMP_DB_PATH #$TEMP_LOG_PATH
   exit
 }
 trap 'siginthandler' EXIT
 
-tail -f $logFile
+fg
+#tail -f $logFile
